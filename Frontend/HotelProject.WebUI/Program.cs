91ff06618b8ac9085f57b9697c3edd53f009e86e
DEV <DEV@DESKTOP-K9AHRHS>
2023-11-01 09:52:20 +0300

    27. Personelin Güncellenmesi

--- Frontend/HotelProject.WebUI/Program.cs
+++ Frontend/HotelProject.WebUI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -26,3 +29,5 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+
