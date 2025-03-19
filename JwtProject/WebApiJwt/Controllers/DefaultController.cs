b7b8707e5f6bfadd8bb58506eb15186879be0ee6
Developer <Developer@DEV>
2025-03-19 15:17:03 +0300

    84. Admin İçin Token Testi

--- JwtProject/WebApiJwt/Controllers/DefaultController.cs
+++ JwtProject/WebApiJwt/Controllers/DefaultController.cs
@@ -10,14 +10,14 @@ namespace WebApiJwt.Controllers
     public class DefaultController : ControllerBase
     {
         [HttpGet("[action]")]
-        public IActionResult Test()
+        public IActionResult TokenOlustur()
         {
             return Ok(new CreateToken().TokenCreate());
         }
         [HttpGet("[action]")]
-        public IActionResult TestAdmin()
+        public IActionResult AdminTokenOlustur()
         {
-            return Ok(new CreateToken().TokenCreate());
+            return Ok(new CreateToken().TokenCreateAdmin());
         }
         [Authorize]
         [HttpGet("[action]")]
