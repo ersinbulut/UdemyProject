02df7e21ad14924bb90ed0a61cd01b55a51e36b7
Developer <Developer@DEV>
2025-05-06 09:11:28 +0300

    148. Indexte Karar Yapısı Kullanmak

--- Frontend/HotelProject.WebUI/Dtos/AppUserDto/ResultAppUserListDto.cs
+++ Frontend/HotelProject.WebUI/Dtos/AppUserDto/ResultAppUserListDto.cs
@@ -6,6 +6,8 @@
         public string Surname { get; set; }
         public string City { get; set; }
         public string ImageUrl { get; set; }
+        public string Country { get; set; }
+        public string Gender { get; set; }
 
     }
 }
